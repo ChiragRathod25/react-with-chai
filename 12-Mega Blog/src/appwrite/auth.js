36a7fa26b31c561888679786cb5382a759b1b9a0
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2025-01-17 19:46:43 +0000

    Add Appwrite integration for user authentication and session management

---
+++ 12-Mega Blog/src/appwrite/auth.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-empty */
+import config from "../conf/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  Client = new Client();
+  account;
+  constructor() {
+    this.Client.setEndpoint(config.appwriteUrl).setProject(
+      config.appwriteProjectId
+    );
+    this.account = new Account(this.Client);
+  }
+
+  async createUser({ email, password, name }) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite serive :: getCurrentUser :: error", error);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+}
+const authService = new AuthService();
+
+export default authService;
